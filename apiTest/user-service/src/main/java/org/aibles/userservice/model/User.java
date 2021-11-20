02705ff1c64a429649d359ac9dd15c3182aaf801
doducanh2002 <82581500+doducanh2002@users.noreply.github.com>
2021-11-20 17:59:57 +0700

    Update User.java

--- apiTest/user-service/src/main/java/org/aibles/userservice/model/User.java
+++ apiTest/user-service/src/main/java/org/aibles/userservice/model/User.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.Objects;
 
 @Entity
 @Table(name = "users")
