eda8fd475a25af11dfbe850353695caf212e7e20
do duc anh <anh15042002hla@gmail.com>
2022-01-06 21:20:52 +0700

    test

--- apiTest/user-service/src/main/java/org/aibles/userservice/exception2/AbstractException.java
+++ apiTest/user-service/src/main/java/org/aibles/userservice/exception2/AbstractException.java
@@ -1,24 +1,24 @@
-package org.aibles.userservice.exception2;
-
-import org.springframework.http.HttpStatus;
-
-public class AbstractException extends RuntimeException {
-    private final String message;
-    private final HttpStatus status;
-
-    public AbstractException(String message, HttpStatus status) {
-        super(message);
-        this.message = message;
-        this.status = status;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-}
+//package org.aibles.userservice.exception2;
+//
+//import org.springframework.http.HttpStatus;
+//
+//public class AbstractException extends RuntimeException {
+//    private final String message;
+//    private final HttpStatus status;
+//
+//    public AbstractException(String message, HttpStatus status) {
+//        super(message);
+//        this.message = message;
+//        this.status = status;
+//    }
+//
+//    @Override
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public HttpStatus getStatus() {
+//        return status;
+//    }
+//
+//}
