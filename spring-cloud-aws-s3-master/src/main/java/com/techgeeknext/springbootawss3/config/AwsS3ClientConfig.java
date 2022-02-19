a53e3cc0365a06e78632bc03271ba5f2ba01425d
do duc anh <anh15042002hla@gmail.com>
2022-02-19 23:05:11 +0700

    s3

---
+++ spring-cloud-aws-s3-master/src/main/java/com/techgeeknext/springbootawss3/config/AwsS3ClientConfig.java
@@ -0,0 +1,32 @@
+package com.techgeeknext.springbootawss3.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsS3ClientConfig {
+
+    @Value("${s3.access.key}")
+    private String awsId;
+
+    @Value("${s3.secret.key}")
+    private String awsKey;
+
+    @Bean
+    public AmazonS3 s3client() {
+
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsId, awsKey);
+        AmazonS3 amazonS3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_SOUTHEAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+
+        return amazonS3Client;
+    }
+}
