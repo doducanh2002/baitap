0c85e16fa17fb9300f1eec1e57c3ecb35450a2b7
doducanh2002 <82581500+doducanh2002@users.noreply.github.com>
2022-09-29 15:02:26 +0700

    Delete demo-car directory

--- demo-car/src/main/java/com/aibles/democar/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aibles.democar.repository;
-
-import com.aibles.democar.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car,Integer> {
-}
