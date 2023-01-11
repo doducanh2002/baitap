cf036f245cdaf052184b4c7ae10f2f7f1dd18969
do duc anh <anh15042002hla@gmail.com>
2023-01-11 19:30:01 +0700

    update: using single response clean code

--- java-core/src/SearchNumber.java
+++ java-core/src/SearchNumber.java
@@ -6,18 +6,24 @@ public class SearchNumber {
     //kiểm tra các số từ 10 đến 200 theo điều kiện chia heets cho 7 và không là bội của 5
     //lưu vào list
     //in ra
-        public static void main(String[] args) {
-            System.out.println("Các số chia hết cho 7 là: ");
+    public static void main(String[] args) {
+        System.out.println("Các số chia hết cho 7 là: ");
+        ArrayList<Integer> list = new ArrayList<>();
+        searchNumber(list);
+        display(list);
+    }
 
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int i= 10;i<=200;i++) {//giới hạn các số trong khoảng từ 10 đến 200 tính cả 10 và 200
-                if((i%7==0) && (i%5!=0)) { // tìm các số chia hết cho 7 và k là bội của 5
-                   list.add(i);//lưu vào list
-                }
-            }
-
-            for (int i = 0; i<list.size() - 1; i++ ){
-                System.out.print(list.get(i)+",");//lấy ra các số theo yêu cầu
+    public static void searchNumber(ArrayList<Integer> list) {
+        for (int index = 10; index <= 200; index++) {//giới hạn các số trong khoảng từ 10 đến 200 tính cả 10 và 200
+            if ((index % 7 == 0) && (index % 5 != 0)) { // tìm các số chia hết cho 7 và k là bội của 5
+                list.add(index);//lưu vào list
             }
         }
+    }
+
+    public static void display(ArrayList<Integer> list) {
+        for (int index = 0; index < list.size() - 1; index++) {
+            System.out.print(list.get(index) + ",");//lấy ra các số theo yêu cầu
+        }
+    }
 }
